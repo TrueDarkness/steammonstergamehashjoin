83c64c762276a4517291ea3d043fe09ab947a053
TrueDarkness <gimmeyourspam@gmail.com>
2015-06-18 12:34:07 -0400

    Update smghj.js

--- smghj.js
+++ smghj.js
@@ -2,9 +2,11 @@
 // @name Hash Join for Steam's Monster Game
 // @namespace https://github.com/TrueDarkness/steammonstergamehashjoin
 // @description A script that runs the Steam Monster Minigame for you.
-// @version 1.0
+// @version 1.1
 // @match *://steamcommunity.com/minigame
 // @match *://steamcommunity.com/minigame/
+// @match *://steamcommunity.com//minigame
+// @match *://steamcommunity.com//minigame/
 // @grant none
 // @updateURL https://github.com/TrueDarkness/steammonstergamehashjoin/raw/master/smghj.js
 // @downloadURL https://github.com/TrueDarkness/steammonstergamehashjoin/raw/master/smghj.js
